cfef72cbe6d11776088bafc7fdcb1e41b3aeeb33
JhonathanAlejandroJalaU <Jhonathan.Angarita@jala.university>
2025-02-26 23:31:57 -0500

    renamed model class

--- SpaceInvadersJA/Models/MysteryEnemy.cs
+++ SpaceInvadersJA/Models/MysteryEnemy.cs
@@ -1,13 +1,13 @@
 
 
 namespace SpaceInvadersJA.Models;
-public class MysteryShip : Enemy
+public class MysteryEnemy : Enemy
 {
     private const double SHIP_WIDTH = 60;
     private const double SHIP_HEIGHT = 60;
     private static readonly Random _random = new Random();
 
-    public MysteryShip(double x, double y)
+    public MysteryEnemy(double x, double y)
         : base("ms-appx:///Assets/Images/mystery.gif", x, y, SHIP_WIDTH, SHIP_HEIGHT, _random.Next(50, 300))
     {
         
